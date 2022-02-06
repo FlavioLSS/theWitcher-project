bc7168bc9275dd9753a2132deadd05923aec6723
Lucas Senna <tarcisio.almeida197@gmail.com>
2022-02-06 15:38:18 -0300

    modal-update

--- script/script.js
+++ script/script.js
@@ -20,3 +20,75 @@ function hideGeralt(){
     test.style.background = '#EAEAEA';
     hideCards.style.display = 'block';
 }
+
+
+function showGeralt2(){
+    let modal = document.querySelector('.modal2')
+    var test = document.querySelector('#cover')
+    var hideCards = document.querySelector('#cards')
+
+    hideCards.style.display = 'none';
+    modal.style.display = 'flex';
+    test.style.background = 'rgba(0, 0, 0, 0.6)';
+    test.style.opacity = '.8';
+    modal.style.opacity = '1';
+}
+
+function hideGeralt2(){
+    let hide = document.querySelector('.modal2')
+    var test = document.querySelector('#cover')
+    var hideCards = document.querySelector('#cards')
+
+    hide.style.display = 'none';
+    test.style.opacity = '1';
+    test.style.background = '#EAEAEA';
+    hideCards.style.display = 'block';
+}
+
+
+function showGeralt3(){
+    let modal = document.querySelector('.modal3')
+    var test = document.querySelector('#cover')
+    var hideCards = document.querySelector('#cards')
+
+    hideCards.style.display = 'none';
+    modal.style.display = 'flex';
+    test.style.background = 'rgba(0, 0, 0, 0.6)';
+    test.style.opacity = '.8';
+    modal.style.opacity = '1';
+}
+
+function hideGeralt3(){
+    let hide = document.querySelector('.modal3')
+    var test = document.querySelector('#cover')
+    var hideCards = document.querySelector('#cards')
+
+    hide.style.display = 'none';
+    test.style.opacity = '1';
+    test.style.background = '#EAEAEA';
+    hideCards.style.display = 'block';
+}
+
+
+function showGeralt4(){
+    let modal = document.querySelector('.modal4')
+    var test = document.querySelector('#cover')
+    var hideCards = document.querySelector('#cards')
+
+    hideCards.style.display = 'none';
+    modal.style.display = 'flex';
+    test.style.background = 'rgba(0, 0, 0, 0.6)';
+    test.style.opacity = '.8';
+    modal.style.opacity = '1';
+}
+
+function hideGeralt4(){
+    let hide = document.querySelector('.modal4')
+    var test = document.querySelector('#cover')
+    var hideCards = document.querySelector('#cards')
+
+    hide.style.display = 'none';
+    test.style.opacity = '1';
+    test.style.background = '#EAEAEA';
+    hideCards.style.display = 'block';
+}
